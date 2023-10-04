4a74f79e7553a9ca86f785c724c05950c39c1ce2
Gene Xu <genexu1117.dev@gmail.com>
2023-10-04 16:45:21 +0800

    Add border rounded style

--- src/components/SearchBar/Container.jsx
+++ src/components/SearchBar/Container.jsx
@@ -1,5 +1,5 @@
 const Container = ({ children, ...rest }) => (
-	<div className="w-full mx-auto md:w-4/5 lg:w-2/5 mt-20 bg-slate-200 dark:bg-slate-800" {...rest}>
+	<div className="w-full mx-auto md:w-4/5 lg:w-2/5 mt-20 bg-slate-200 dark:bg-slate-800 rounded-md" {...rest}>
 		{children}
 	</div>
 );
